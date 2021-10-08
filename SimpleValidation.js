b988419920fd421ac7e4d6e74b8b048bf1f91892
aliakseikulba <kulba.av@gmail.com>
2021-10-08 10:53:15 +0300

    fix validation task

--- SimpleValidation.js
+++ SimpleValidation.js
@@ -5,6 +5,18 @@ numbers,
 underscore
 Length should be between 4 and 16 characters (both included).*/
 
+
+/*
+ - `^`        Start from the beginning of the string.
+ - `[]`       Allow any character specified, including...
+ - `a-z`      anything from a to z,
+ - `0-9`      anything from 0 to 9,
+ - `_`        and underscore.
+ - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+ - `$`        End the string right after specified amount of allowed characters is given.
+ */
+
+const validator = /^[a-z0-9_]{4,16}$/;
 function validateUsr(username) {
   res =  /^[a-z0-9_]{4,16}$/.test(username)       //regex here/.test(username)
   return res

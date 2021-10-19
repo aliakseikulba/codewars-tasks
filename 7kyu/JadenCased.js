12d3e4d22cac438775f244a1ece8cb25050b5f21
aliakseikulba <kulba.av@gmail.com>
2021-10-19 12:02:54 +0300

    fixed

--- 7kyu/JadenCased.js
+++ 7kyu/JadenCased.js
@@ -7,3 +7,11 @@ String.prototype.toJadenCase = function () {
   }
   return jCased.join(' ');
 }
+
+/*
+String.prototype.toJadenCase = function () {
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
+*/

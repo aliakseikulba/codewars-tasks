08ece653fbf70b758d7b480cad56f214b155599c
aliakseikulba <kulba.av@gmail.com>
2021-10-19 12:01:14 +0300

    toUpperCase first letter in the word casse

---
+++ 7kyu/JadenCased.js
@@ -0,0 +1,9 @@
+String.prototype.toJadenCase = function () {
+  let jCased = [];
+  let words = this.toLowerCase().split(' ');
+  for(let i = 0; i < words.length; i++){
+    let word = words[i];
+    jCased.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return jCased.join(' ');
+}
